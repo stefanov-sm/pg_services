a72b1be23acabef02a151fd51043f14ef8e14ddd
Stefan Stefanov <stefanov.sm@abv.bg>
2023-12-03 12:41:58 +0200

    Delete services/queries/b_service.sql

--- services/queries/b_service.sql
+++
@@ -1,5 +0,0 @@
-select jsonb_build_object('arr', jsonb_agg(to_json(t)), 'scalar', null) from
-(
- select seq running_number, to_char(seq, 'FMRN') as_roman_numeral
- from generate_series((:arg ->> 'lower_limit')::int, (:arg ->> 'upper_limit')::int, 1) seq
-) t;
