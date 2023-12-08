b3a69e4b006988de490ee494306140fc2893b15d
Stefan Stefanov <stefanov.sm@abv.bg>
2023-12-08 12:56:25 +0200

    Add files via upload

--- config/log.table.sql
+++ config/log.table.sql
@@ -1,8 +1,9 @@
 create table tests.pg_services_log 
 (
- call_time timestamptz not null default current_timestamp,
+ call_time timestamp with time zone not null default current_timestamp,
+ service_name text not null,
  call_by text not null,
- call_resource text not null,
+ call_method text not null,
  call_payload text not null,
- constraint pg_services_log_pk primary key (call_resource, call_by, call_time)
+ constraint pg_services_log_pk primary key (service_name, call_method, call_time)
 );
